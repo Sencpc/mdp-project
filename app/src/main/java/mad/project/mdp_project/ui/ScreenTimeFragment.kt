2075b1a4dc04e6288e854f1c3d1cb437797a3717
Sebastian Tjandra <sebastiantjandra1@gmail.com>
2026-07-06 21:06:03 +0700

    Screen Time UI placeholder

--- app/src/main/java/mad/project/mdp_project/ui/ScreenTimeFragment.kt
+++ app/src/main/java/mad/project/mdp_project/ui/ScreenTimeFragment.kt
@@ -5,11 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import mad.project.mdp_project.databinding.FragmentSleepTrackerBinding // Menggunakan binding yang mirip sementara
+import mad.project.mdp_project.databinding.FragmentScreenTimeBinding
 
 class ScreenTimeFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Placeholder view sederhana
-        return View(context)
+    private var _binding: FragmentScreenTimeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentScreenTimeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        // Here you can initialize values or set click listeners
+        binding.btnNotification.setOnClickListener {
+            // Handle notification click
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
