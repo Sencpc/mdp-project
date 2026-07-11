3c45f79e1d7ebf4c864190a484098018f7ab817b
Sen <seancornelius11@gmail.com>
2026-07-11 15:31:45 +0700

    port fix

--- WebServices/db.js
+++ WebServices/db.js
@@ -9,7 +9,7 @@ const DB_PORT = process.env.DB_PORT;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
-  port: DB_PORT,
+  port: DB_PORT || 3306,
   dialect: "mysql",
   logging: false,
 });
